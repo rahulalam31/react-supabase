06d060b44fa776dd0e33841d9475cfbcf075f19f
rahulalam31 <gaberialcramern31@outlook.com>
2024-05-07 15:09:22 +0530

    client js

--- src/createClient.js
+++ src/createClient.js
@@ -1,1 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://tulgpnqafyrqlodcwtkp.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1bGdwbnFhZnlycWxvZGN3dGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5NjgyNjAsImV4cCI6MjAxMTU0NDI2MH0.cHnUty1tqkF2_4AxvKIRngK_CrLVU9kuo92jizovrUI")
